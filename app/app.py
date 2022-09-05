5e8984228fa39539f352b0a47d1000a69a89f6f8
thisIsShikhar <shikharali13@gmail.com>
2022-09-05 14:40:41 +0600

    refactored the template a bit, created the model and Database

--- app/app.py
+++ app/app.py
@@ -1,6 +1,25 @@
+from email.policy import default
+import sqlite3
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 import os
+from datetime import datetime
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACT_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200), primary_key = True)
+    desc = db.Column(db.String(500), primary_key = True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
 
 @app.route('/')
 def hello_world():
